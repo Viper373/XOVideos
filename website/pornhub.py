bb0a9d91d92e8806d8081be17a6eb8e4960025b4
Viper373 <2020311228@bipt.edu.cn>
2025-02-26 16:36:19 +0800

    🧨移除load_cookies方法，改为github变量获取

--- website/pornhub.py
+++ website/pornhub.py
@@ -104,7 +104,7 @@ class Pornhub:
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
         }
-        self.cookies = string_utils.load_cookies(ph_config.get('Cookies', 'COOKIES'))
+        self.cookies = os.getenv('PH_COOKIES')
         self.proxies = proxy_utils.get_proxy()
 
     @rich_logger
